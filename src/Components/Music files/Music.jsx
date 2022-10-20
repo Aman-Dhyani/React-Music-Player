976e750f27861be07c3e518af05a6cde53bcfeff
Aman Dhyani <102688642+Aman-Dhyani@users.noreply.github.com>
2022-10-20 03:36:02 -0700

    Update Music.jsx

--- src/Components/Music files/Music.jsx
+++ src/Components/Music files/Music.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react'
+import React, { useCallback, useReducer, useState } from 'react'
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import musicData from '../json/musicData.json'
@@ -6,38 +6,52 @@ import './Music.css'
 
 export default function Music(props) {
 
-  // ----- reducer hook for count ---------
-  function reducer(count, action) {
+  const reducer = useCallback((count, action) => {
     if (action.type === "prev") return count > 0 ? count - 1 : 9
     if (action.type === "next") return count < 9 ? count + 1 : 0
     if (action.type === "suffleCount") return count = Math.floor(Math.random() * 10)
-  }
+  }, [])
 
   const [count, dispatch] = useReducer(reducer, 0)
-
-  // ----- state hook ----------- 
   const [anime, updateAnime] = useState('paused')
   const [anime2, updateAnime2] = useState('')
+
   const artistImage = musicData.music[count].artistPic
   const songName = musicData.music[count].songName
   const albumName = musicData.music[count].album
   const artistName = musicData.music[count].artist
+
+  // const audio = musicData.music[count].song -- json issue that why i,m using manually imported music by MusicData.jsx
   const newAudios = props.musicArr
+
+  console.log("aman");
 
   return (
     <>
       <div id='MusicPage'>
         <div id='backCont' style={{ backgroundImage: `url(${artistImage})` }} ></div>
         <div id='MusicContainerDiv'>
-          <AudioPlayer src={newAudios[count]} /*src={audio}*/ onPause={() => updateAnime('paused')} onPlay={() => updateAnime('') || updateAnime2('rotating')} />
+
+          <AudioPlayer /*src={audio}*/ src={newAudios[count]}
+            onPause={() => updateAnime('paused')}
+            onPlay={() => updateAnime('') || updateAnime2('rotating')} />
+
           <div className='image-Cont' style={{ backgroundImage: `url(${artistImage})`, animation: `${anime2} 10s linear infinite ${anime}` }}>
-            <div id='musicLogo' style={{ animation: `${anime2} 10s linear infinite reverse ${anime}` }} ></div>
+            <div id='musicLogo' style={{ animation: `${anime2} 10s linear infinite reverse ${anime}` }}></div>
           </div>
+
           <div id='artistName' ><h1>{artistName}</h1></div>
           <div id='songName' ><h3>{songName}</h3></div>
           <div id='album' ><h2>{albumName}</h2></div>
-          <button className='navigaterBtn' id='prev' onClick={() => updateAnime2('') || dispatch({ type: "prev" })}></button>
-          <button className='navigaterBtn' id='next' onClick={() => updateAnime2('') || dispatch({ type: "next" })}></button>
+
+          <button
+            className='navigaterBtn' id='prev' onClick={() => updateAnime2('') || dispatch({ type: "prev" })}>
+          </button>
+
+          <button
+            className='navigaterBtn' id='next' onClick={() => updateAnime2('') || dispatch({ type: "next" })}>
+          </button>
+
           <div id='suffle' onClick={() => dispatch({ type: "suffleCount" })}></div>
         </div>
       </div>
